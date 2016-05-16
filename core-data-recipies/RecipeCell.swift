0ec22e400f0fae9993404ee6531da3b0d2b9f456
adrpadua <adrpadua@gmail.com>
2016-05-16 08:56:27 -0700

    TableView updates through RecipeCell class

---
+++ core-data-recipies/RecipeCell.swift
@@ -0,0 +1,25 @@
+//
+//  RecipeCell.swift
+//  core-data-recipies
+//
+//  Created by Adrian Padua on 5/16/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(recipe: Recipe) {
+        recipeTitle.text = recipe.title
+        recipeImg.image = recipe.getRecipeImg()
+    }
+}
