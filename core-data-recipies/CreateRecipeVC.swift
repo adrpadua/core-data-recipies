9ea748f68d0bbc01c8bb71d252beb8be15792c3c
adrpadua <adrpadua@gmail.com>
2016-05-16 10:01:10 -0700

    added CreateRecipeVC, and implemented saving through CoreData.

---
+++ core-data-recipies/CreateRecipeVC.swift
@@ -0,0 +1,65 @@
+//
+//  CreateRecipeVC.swift
+//  core-data-recipies
+//
+//  Created by Adrian Padua on 5/16/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CreateRecipeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var recipeTitle: UITextField!
+    @IBOutlet weak var recipeIngredients: UITextField!
+    @IBOutlet weak var recipeSteps: UITextField!
+    @IBOutlet weak var recipeImg: UIImageView!
+    @IBOutlet weak var addRecipeBtn: UIButton!
+    
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        recipeImg.layer.cornerRadius = 4.0
+        recipeImg.clipsToBounds = true
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        recipeImg.image = image
+    }
+    
+    @IBAction func addImageBtn(sender: AnyObject) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func createRecipe(sender: AnyObject) {
+        if let title = recipeTitle.text where title != "" {
+            
+            let app = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context = app.managedObjectContext
+            let entity = NSEntityDescription.entityForName("Recipe", inManagedObjectContext: context)
+            let recipe = Recipe(entity: entity!, insertIntoManagedObjectContext: context)
+            
+            recipe.title = recipeTitle.text
+            recipe.ingredients = recipeIngredients.text
+            recipe.steps = recipeSteps.text
+            recipe.setRecipeImage(recipeImg.image!)
+            
+            context.insertObject(recipe)
+            
+            do {
+                try context.save()
+            } catch {
+                print("could not save")
+            }
+            
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+}
